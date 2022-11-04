12d087594802fcc7fb2f72e84abb74011c610d89
Mikel <mikel362d@gmail.com>
2022-11-03 17:22:37 -0700

    gets props from redux store through connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
     render() {
+        //this.props === { songs: state.songs }
         return <div>SongList</div>
     };
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
